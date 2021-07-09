2d240db25ce342feac96d6eaeafd3dbcd7c3fca5
ericklf91 <erick.lemosferreira@gmail.com>
2021-07-09 19:11:12 -0300

    Testes navigation

---
+++ src/pages/cart/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import styles from './styles';
+
+
+const CartScreen = () => {
+    return (
+        <View>
+            <Text>Cart Screen</Text>
+        </View>
+    );
+}
+
+export default CartScreen;
---
+++ src/pages/checkout/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import styles from './styles';
+
+
+const CheckoutScreen = () => {
+    return (
+        <View>
+            <Text>Checkout Screen</Text>
+        </View>
+    );
+}
+
+export default CheckoutScreen;
