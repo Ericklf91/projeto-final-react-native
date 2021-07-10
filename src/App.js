282ab0fcfbb4cfafbbfcf1081e791cff25b3cb36
ericklf91 <erick.lemosferreira@gmail.com>
2021-07-10 14:26:55 -0300

    Inserindo requisições à API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Provider as PaperProvider } from 'react-native-paper';
 import MainDrawer from './navigators/drawer';
 
 const App = () => {
     return (
-        <NavigationContainer>
-            <MainDrawer />
-        </NavigationContainer>
+        <PaperProvider>
+            <NavigationContainer>
+                <MainDrawer />
+            </NavigationContainer>
+        </PaperProvider>
     );
 }
 
--- src/pages/category/index.js
+++ src/pages/category/index.js
@@ -1,21 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, ActivityIndicator } from 'react-native';
+import { View, Text, FlatList, ActivityIndicator, Button, Alert } from 'react-native';
+import { Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Dialog, Portal } from 'react-native-paper';
+import api from '../../services/api';
 import styles from './styles';
 
-
 const CategoryScreen = () => {
-    const [categorias, setCategorias] = useState([]);
+    const [categories, setCategories] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [nome, setNome] = useState('');
+    const [desc, setDesc] = useState('');
+
+    const [visible, setVisible] = useState(false);
+
+    const showDialog = () => setVisible(true);
+
+    const hideDialog = () => setVisible(false);
 
     useEffect(() => {
-        loadCategorias();
-      }, []);
+        loadCategories();
+    }, []);
 
-    const loadCategorias = () => {
-        setCategorias([{
-            nome: "Teste",
-            descricao: "Teste"
-        }]);
+    const loadCategories = () => {
+        const headers = {
+            "X-Authorization": "pk_test_30111d23b155b7b570eb490bfbff8f60ede41335fefbe"
+        };
+        api.get(`/categories`, { headers })
+            .then((response) => {
+                setCategories(response.data.data);
+            }).catch(function (error) {
+                alert(error);
+            });
+    }
+
+    const insertCategories = () => {
+        const headers = {
+            "X-Authorization": "sk_30111510d2a5c7c20fcb74987aee0ee376f1d911a9118",
+            "Content-Type": "application/json"
+        };
+        const slug = (nome + 1);
+        const content = { name: nome, description: desc, slug: slug }
+        api.post(`/categories`, content, { headers })
+            .then(() => {
+                Alert.alert('Categoria criada');
+                loadCategories();
+                hideDialog();
+            }).catch(function (error) {
+                alert(error);
+            });
+    }
+
+    const deleteCategorie = (id) => {
+        const headers = {
+            "X-Authorization": "sk_30111f852eeb033c9341a70ca0ba06287f5da000050c6"
+        };
+        api.delete(`/categories/${id}`, { headers })
+            .then(() => {
+                Alert.alert('Categoria deletada')
+                loadCategories();
+            });
     }
 
     const renderFooter = () => {
@@ -29,20 +73,53 @@ const CategoryScreen = () => {
     }
 
     return (
-        <FlatList style={styles.list}
-            onEndReachedThreshold={0.1}
-            ListFooterComponent={renderFooter}
-            data={categorias}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item, index }) => {
-                return (
-                    <View>
-                        <Text style={styles.listItem}>Nome: {item.nome}</Text>
-                        <Text style={styles.listItem}>Descrição: {item.descricao}</Text>
-                    </View>
-                )
-            }}
-        />
+        <View>
+            <Button title="Inserir Categoria" onPress={showDialog} />
+            <Portal>
+                <Dialog visible={visible} onDismiss={hideDialog}>
+                    <Dialog.Title>Inserir categoria</Dialog.Title>
+                    <Dialog.Content>
+                        <Input
+                            value={nome}
+                            onChangeText={(t) => setNome(t)}
+                            leftIcon={<Icon
+                                name='folder'
+                                size={24}
+                                color='black'
+                            />}
+                            label="Nome" placeholder="Nome" />
+                        <Input
+                            value={desc}
+                            onChangeText={(t) => setDesc(t)}
+                            leftIcon={<Icon
+                                name='file'
+                                size={24}
+                                color='black'
+                            />}
+                            label="Descrição" placeholder="Descrição" />
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button title="Cancelar" onPress={hideDialog} />
+                        <Button title="Enviar" onPress={insertCategories} />
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
+            <FlatList style={styles.list}
+                onEndReachedThreshold={0.1}
+                ListFooterComponent={renderFooter}
+                data={categories}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item, index }) => {
+                    return (
+                        <View>
+                            <Text style={styles.listItem}>Nome: {item.name}</Text>
+                            <Text style={styles.listItem}>Descrição: {item.description}</Text>
+                            <Button title="Deletar" onPress={() => deleteCategorie(item.id)} />
+                        </View>
+                    )
+                }}
+            />
+        </View>
     );
 }
 
